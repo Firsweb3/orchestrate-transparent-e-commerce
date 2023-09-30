f5ddfde056e0cf4c25d117fc2ab2599168de7b25
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-09-30 15:41:56 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,72 +1,27 @@
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = generateRandomData();
-function main() {
-    const data = [];
-
-
-    }
-function generateRandomData() {
-    });
-main();
-    for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
-
-    return data;
-}
-        console.log("Random Number: " + item);
-}def main():
-
-
-    main()
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    data = generate_random_data()
-
-if __name__ == "__main__":
     return data
-    for item in data:
-
-        print(f"Random Number: {item}")function main() {
-    data.forEach(item => {
-
-main();
-    return data;
-    }
-        console.log("Random Number: " + item);
-    });
-function generateRandomData() {
-}
-
-    const data = generateRandomData();
-}
-
-    const data = [];
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {def main():
-    data = generate_random_data()
-
-
-import random
-if __name__ == "__main__":
         print(f"Random Number: {item}")
-    return data
-def generate_random_data():
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
-    for item in data:import random
-
 if __name__ == "__main__":
+def generate_random_data():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
 def main():
 
     for item in data:
+
+
     data = generate_random_data()
-    return data
 
-        print(f"Random Number: {item}")
+    main()    for item in data:
+import random
 
-def generate_random_data():
-    main()
+def main():
+
     data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+
+    main()
+    data = generate_random_data()
+def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
