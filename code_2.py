1e6b07fb1b9bef4079fbff6c9a953d26d6be4592
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-10-02 23:09:11 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,53 +1,42 @@
-    for item in data:
-if __name__ == "__main__":
-    main()
+            data[i] = (int) (Math.random() * 100) + 1;
 
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+        }
+        }
+    }
+
+            System.out.println("Random Number: " + item);
+}
+public class RandomDataGenerator {
+        int[] data = new int[10];
+    public static void main(String[] args) {function generateRandomData() {
+
+}
+
+    });
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+main();
+    const data = generateRandomData();
+    return data;
+function main() {
+
+        console.log("Random Number: " + item);
+    const data = [];
+}def generate_random_data():
+def main():
+    main()
+import random
 
         print(f"Random Number: {item}")
     data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
 
-    return data
-def generate_random_data():
-def main():
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
+
 if __name__ == "__main__":
-        print(f"Random Number: {item}")
-
-def main():
-    main()
-    data = generate_random_data()
-
-
-
-import random
-if __name__ == "__main__":
-
-
-    return data
-    main()
-
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-def generate_random_data():
-def main():
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    main()
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-def main():
-import random
-
     for item in data:
 
-    data = generate_random_data()
     return data
