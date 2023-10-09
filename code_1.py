987beafd131a1bc0c7106c8493d82963f297b1ce
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-10-09 21:35:26 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,30 +1,30 @@
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
 def main():
-    for item in data:
-        print(f"Random Number: {item}")
+    main()
 
     return data
-
-
-    data = generate_random_data()
-
 def generate_random_data():
-    main()
-import random
-        console.log("Random Number: " + item);
+    data = generate_random_data()
+if __name__ == "__main__":
+
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+import random    for (let i = 0; i < 10; i++) {
 }
-function main() {
-    });
-}
-    data.forEach(item => {
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
     }
 
 
-    for (let i = 0; i < 10; i++) {
-    const data = generateRandomData();
-function generateRandomData() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
-main();
     const data = [];
+    data.forEach(item => {
+    });
+function generateRandomData() {
+        console.log("Random Number: " + item);
+function main() {
+    return data;
+
+}
